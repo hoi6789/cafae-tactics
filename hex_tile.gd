a842022fc1fb7634cee6c1c650402302c6b1b97a
hoi6789 <poo_hoi@hotmail.com>
2026-01-07 21:43:33 -0500

    new hexagon tiling system

--- hex_tile.gd
+++ hex_tile.gd
@@ -1,8 +1,25 @@
 extends StaticBody3D
 var id = 0
+var q: float = 0
+var r: float = 0
+var s: float = 0
+
+func setPosition(cubePos: Vector2):
+	q = cubePos.x
+	r = cubePos.y
+	s = 0 - q - r
+	
+	var offset_q = q / 2 * Vector3(sqrt(3), 0, 0)
+	var offset_r = r / 2 * Vector3(sqrt(3)/2, 0, 1.5)
+	
+	position = offset_q + offset_r
+	print(position)
+	pass
 
 func _on_mouse_entered() -> void:
-	$CollisionPolygon3D/MeshInstance3D.get_surface_override_material(0).albedo_color = Color(0, 1, 0, 1)
+	var newSurface = $CollisionPolygon3D/MeshInstance3D.get_surface_override_material(0).duplicate(true)
+	newSurface.albedo_color = Color(0, 1, 0, 1)
+	$CollisionPolygon3D/MeshInstance3D.set_surface_override_material(0, newSurface)
 	print(id)
 	pass # Replace with function body.
 
